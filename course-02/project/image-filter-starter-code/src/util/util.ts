f4a93c720b402726a8fc1595c93876f3e9344a35
suren2020 <45539434+suren2020@users.noreply.github.com>
2019-12-08 17:34:59 -0500

    Implemented code for the project "Your Own Instagram on AWS".

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
 import Jimp = require('jimp');
+const path = require('path');
+const slash = require('slash');
+const regex = require('regex');
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -12,12 +15,19 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
     return new Promise( async resolve => {
         const photo = await Jimp.read(inputURL);
         const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+
+        var re = /(^[a-zA-Z]:)(.*)/;
+        var str = slash(__dirname);
+        var dirname = str.replace(re, '$2');
+        // console.log("dirname = " + dirname);
+
         await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
+        // .write(__dirname+outpath, (img)=>{
+        .write(dirname+outpath, (img)=>{
+            resolve(dirname+outpath);
         });
     });
 }
@@ -27,8 +37,17 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
+// export async function deleteLocalFiles(files:Array<string>){
+  export async function deleteLocalFiles(file: string){
+      // console.log("deleteLocalFiles 111 - file = " + file);
+
+      fs.unlinkSync(file);
+      // console.log("deleteLocalFiles 222 - file = " + file);
+
 }
+
+// export async function sendResponse(res: Response, file:string) {
+//     res.status(200)
+//        .sendFile(file);
+//       console.log("sendResponse- file = " + file);
+//}
